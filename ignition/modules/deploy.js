763d6ba28e18d7a0b1da6361c00eb8fa421e0067
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-11-25 09:05:58 +0200

    start project

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -16,6 +16,47 @@ async function main() {
   console.log("Deploying with", await deployer.getAddress());
 
   const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer);
-  const auction = await DutchAuction.deploy();
+  const auction = await DutchAuction.deploy(
+    ethersutils.parseEther('2.0'),
+    1,
+    "Motobike"
+  );
   await auction.deployed();
+
+  saveFrontendFiles({
+    DutchAuction: auction
+  });
 }
+
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.join(_dirname, '/..', 'front/contract');
+
+  if(!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract] = contract_item;
+
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + "-contract-address.json" ),
+        JSON.stringify({[name]: contract.address}, undefined, 2)
+      )
+    };
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name);
+    
+    fs.writeFileSync(
+      path.join(contractsDir, "/", name + ".json"),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  });
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
